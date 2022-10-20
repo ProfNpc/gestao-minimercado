c7aa59229ec978c1eec45810758add8e44c00ecc
Alessandro <professornpc@gmail.com>
2022-10-20 20:13:02 -0300

    Projeto Base

---
+++ src/main/java/com/belval/gestaominimercado/GestaominimercadoApplication.java
@@ -0,0 +1,13 @@
+package com.belval.gestaominimercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaominimercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaominimercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belval/gestaominimercado/GestaominimercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belval.gestaominimercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaominimercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
