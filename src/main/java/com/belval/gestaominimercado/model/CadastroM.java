5b856a686a2eefda03d1587be214237c93d3693a
Pietro916 <pietrorantin916@gmail.com>
2022-11-10 20:39:05 -0300

    Getters & Setters att

--- src/main/java/com/belval/gestaominimercado/model/CadastroM.java
+++ src/main/java/com/belval/gestaominimercado/model/CadastroM.java
@@ -8,43 +8,77 @@ public class CadastroM {
 	private String endereco;
 	private String tipo; /*mercado*/
 	private int telefone; /*contato*/
+	private String cnpj;
 	
+	public CadastroM(String fantasia, String email, String senha, String endereco, String tipo, int telefone, String cnpj) {
+		super();
+		this.fantasia = fantasia;
+		this.email = email;
+		this.senha = senha;
+		this.endereco = endereco;
+		this.tipo = tipo;
+		this.telefone = telefone;
+		this.cnpj = cnpj;
+		
+		// TODO Auto-generated constructor stub
+	}
+
 	public String getFantasia() {
 		return fantasia;
 	}
+
 	public void setFantasia(String fantasia) {
 		this.fantasia = fantasia;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public int getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(int telefone) {
 		this.telefone = telefone;
 	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
 	
 	
 
