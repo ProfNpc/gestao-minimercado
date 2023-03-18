d71e8dd4acfbcde65cbf545d19f34e848acc7e23
Nicolas-2701 <ngalvao388@gmail.com>
2023-03-18 15:12:10 -0300

    Lista e cadastro de produtos

--- src/main/java/com/belval/gestaominimercado/repository/ProdutoRepository.java
+++ src/main/java/com/belval/gestaominimercado/repository/ProdutoRepository.java
@@ -3,11 +3,11 @@ package com.belval.gestaominimercado.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.belval.gestaominimercado.model.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
 	  List<Produto> findByDescricao(String descricao);
 
---
+++ src/main/java/com/belval/gestaominimercado/service/ProdutoService.java
@@ -0,0 +1,24 @@
+package com.belval.gestaominimercado.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.belval.gestaominimercado.model.Produto;
+import com.belval.gestaominimercado.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	public Produto cadastrar(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	public List<Produto> listar() {
+	    return produtoRepository.findAll();
+	  }
+}
