8d4fd313cd2c3554fdc65d0b1b24c4c72038b4bb
Nicolas-2701 <ngalvao388@gmail.com>
2023-04-27 18:00:29 -0300

    latt

--- src/main/java/com/belval/gestaominimercado/service/ItemCarrinhoService.java
+++ src/main/java/com/belval/gestaominimercado/service/ItemCarrinhoService.java
@@ -1,10 +1,23 @@
 package com.belval.gestaominimercado.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.belval.gestaominimercado.model.ItemCarrinho;
+import com.belval.gestaominimercado.repository.ItemCarrinhoRepository;
 
-public interface ItemCarrinhoService {
-	ItemCarrinho save (ItemCarrinho itemCarrinho);
-	List<ItemCarrinho> listarItensCarrinho();
+public class ItemCarrinhoService {
+	@Autowired
+	private ItemCarrinhoRepository itemCarrinhoRepository;
+
+	public List<ItemCarrinho> findAll() {
+		return itemCarrinhoRepository.findAll();
+	}
+
+	public ItemCarrinho findById(Long id) {
+		Optional<ItemCarrinho> obj = itemCarrinhoRepository.findById(id);
+		return obj.get();
+	}
 }
